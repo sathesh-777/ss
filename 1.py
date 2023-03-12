c9131030118e2e5bc1f840413200627ad94441bd
SATHESH P C <110752666+sathesh-777@users.noreply.github.com>
2023-03-12 14:06:36 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,27 @@
+# suma onnu
+
+# a=1
+# b=2
+# z=23232
+# u="sayhskdgdh"
+# c=a+b
+# print(c-3)
+# print("satheshkumar")
+# print("sathesh \n kumar")
+
+
+# a=input("enter the number1:")
+# b=input("enter the number2:")
+# c=int(a)+int(b)
+# print("number is",c)
+
+a=input("enter the number1:")
+b=input("enter the number2:")
+c=int (a)+int (b)
+print("number is",c)
+
+
+
+condition
+
+
